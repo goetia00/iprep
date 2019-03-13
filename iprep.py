fc6616bde0f0b0ab5f7d2ef4ffb6d123bf6b568a
Marcel Gerardino <mgerardino@sentineldr.com>
2019-03-13 18:37:10 +0100

    Update iprep.py

--- iprep.py
+++ iprep.py
@@ -11,17 +11,23 @@ def main(args):
 	if len(sys.argv) > 1:
 		ipregx = re.compile('^([0-9]{1,3}\.){3}[0-9]{1,3}$')
 		ip = ipregx.match(sys.argv[1])
+		torl = ""
 		if ip:
 			try:
-				r1 = req.urlopen('https://otx.alienvault.com/api/v1/indicators/IPv4/' + ip[0] + '/reputation')
+				r1 = req.urlopen('https://otx.alienvault.com/api/v1/indicators/IPv4/' + ip[0] + '/reputation', timeout=5)
 				d1 = json.loads(r1.read().decode())
 			except:
 				print("Error occured. Please try again.")
 				sys.exit(1)
+			try:
+				r2 = req.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1', timeout=5)
+				torl = '\n'.join(r2.read().decode().split('\n')[3:])
+			except:
+				pass
 			rep = d1["reputation"]
 			if rep:
-				repd = {"address": "", "threat_score": "", "city": "", "country": "", "organization": "", "first_seen": "", \
-				"last_seen": "","matched_bl": ""}
+				repd = {"address": "", "threat_score": "", "city": "N/A", "country": "N/A", "organization": "N/A", "first_seen": "N/A", \
+				"last_seen": "N/A","matched_bl": ['None'], "Tor exit node: ": "N/A"}
 				for key in rep:
 					if key in repd:
 						repd[key] = rep[key]
@@ -35,17 +41,15 @@ def main(args):
 			else:
 				print("No reputation info found")
 				sys.exit(1)
-			try:
-				r2 = req.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1')
-				torl = '\n'.join(r2.read().decode().split('\n')[3:])
-			except:
-				sys.exit(1)
-			if ip[0] in torl:
-				print("Tor exit node: yes")
+			if torl is not "":
+				if ip[0] in torl:
+					print("Tor exit node: Yes")
+				else:
+					print("Tor exit node: No")
 			else:
-				print("Tor exit node: no")
+				print("Tor exit node: N/A")
 		else:
-			print("Need an IP address as argument.")
+			print("Address specified is invalid.")
 	else:
 		print("Need an IP address as argument.")
 
