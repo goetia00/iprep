85c64a9352e68c6859feda3dbc27eb1522fef38f
Marcel Gerardino <mgerardino@sentineldr.com>
2020-07-21 10:07:26 -0400

    Update iprep.py

--- iprep.py
+++ iprep.py
@@ -1,8 +1,9 @@
 '''
-goetia@sentineldr.com
+goetia@mindcore.es
 simple and quick OSINT about ipv4 addresses
 '''
 import urllib.request as req
+import ssl
 import re
 import json
 import sys
@@ -13,41 +14,33 @@ def main(args):
 		ip = ipregx.match(sys.argv[1])
 		torl = ""
 		if ip:
+			context=ssl._create_unverified_context()
+			repd = [{"otx": {}}, {"tor": 0}]
+			d1 = {}
 			try:
-				r1 = req.urlopen('https://otx.alienvault.com/api/v1/indicators/IPv4/' + ip[0] + '/reputation', timeout=5)
+				r1 = req.urlopen('https://otx.alienvault.com/api/v1/indicators/IPv4/' + ip[0] + '/reputation', context=context, timeout=5)
 				d1 = json.loads(r1.read().decode())
-			except:
-				print("Error occured. Please try again.")
-				sys.exit(1)
+			except Exception as e:
+        			print(str(e))
 			try:
-				r2 = req.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1', timeout=5)
+				r2 = req.urlopen('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1', context=context, timeout=5)
 				torl = '\n'.join(r2.read().decode().split('\n')[3:])
-			except:
-				pass
-			rep = d1["reputation"]
-			if rep:
-				repd = {"address": "", "threat_score": "", "city": "N/A", "country": "N/A", "organization": "N/A", "first_seen": "N/A", \
-				"last_seen": "N/A","matched_bl": ['None'], "Tor exit node: ": "N/A"}
-				for key in rep:
-					if key in repd:
-						repd[key] = rep[key]
-				print("Address: " + repd["address"])
-				print("Threat score: " + str(repd["threat_score"]) + " (out of 7)")
-				print("Location: " + str(repd["city"]) + ", " + repd["country"])
-				print("Organization: " + repd["organization"])
-				print("First seen: " + repd["first_seen"].replace('T',', '))		
-				print("Last seen: " + repd["last_seen"].replace('T',', '))
-				print("Blacklists: " + ' '.join(repd["matched_bl"]))
-			else:
-				print("No reputation info found")
-				sys.exit(1)
+			except Exception as e:
+        			print(str(e))			
+				
+			if 'reputation' in d1 and d1['reputation'] is not None:
+        			rep = d1["reputation"]
+        			repd[0]["otx"] = {"address": "", "threat_score": "", "city": "N/A", "country": "N/A", "organization": "N/A", "first_seen": "N/A", \
+        			"last_seen": "N/A", "matched_bl": ['None']}
+        			for key in rep:
+            				if key in repd[0]["otx"]:
+                				repd[0]["otx"][key] = rep[key]	
 			if torl is not "":
 				if ip[0] in torl:
-					print("Tor exit node: Yes")
+					repd[1]["tor"] = 'yes'
 				else:
-					print("Tor exit node: No")
-			else:
-				print("Tor exit node: N/A")
+					repd[1]["tor"] = 'No'
+			print(json.dumps(repd))
 		else:
 			print("Address specified is invalid.")
 	else:
